ef4c5ff90471eb9db8e28b3946111a7f3150638d
MatheusOliveiraSantos <matheuso.santos@hotmail.com>
2021-07-15 17:29:12 -0300

    alteração no app.js

--- semana5/feed-insta4/insta4/src/App.js
+++ semana5/feed-insta4/insta4/src/App.js
@@ -11,24 +11,36 @@ const MainContainer = styled.div`
 `;
 
 class App extends React.Component {
+  state = {
+    post: [
+      {
+        nome: "Paulinha",
+        fotoUsuario: "https://picsum.photos/50/50", 
+        fotoPost: "https://picsum.photos/200/150"
+      }, 
+      {
+        nome: "Claudinha", 
+        fotoUsuario: "https://i.picsum.photos/id/1/50/50.jpg?hmac=uD_AuvYRKIjw0o41oBnGz4vODQQKOL4EuhDQbxl26Ys", 
+        fotoPost: "https://i.picsum.photos/id/1/200/150.jpg?hmac=9ygk6_GMxpMe_7GpjyUAzAbLNSwk9jG9XLhR6UbFG6c"
+      }, 
+      {
+        nome: "Robertinho", 
+        fotoUsuario: "https://www.artmajeur.com/medias/standard/c/h/christelleferron/artwork/10638688_le-monde-en-couleur-50x50.jpg", 
+        fotoPost: "https://www.fas.org.sg/wp-content/uploads/2018/05/200x150.png"
+      }
+    ]
+  }
+
   render() {
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={"paulinha"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-        <Post
-          nomeUsuario={"claudinha"}
-          fotoUsuario={"https://i.picsum.photos/id/1/50/50.jpg?hmac=uD_AuvYRKIjw0o41oBnGz4vODQQKOL4EuhDQbxl26Ys"}
-          fotoPost={"https://i.picsum.photos/id/1/200/150.jpg?hmac=9ygk6_GMxpMe_7GpjyUAzAbLNSwk9jG9XLhR6UbFG6c"}
-        />
-        <Post
-          nomeUsuario={"robertinho"}
-          fotoUsuario={"https://www.artmajeur.com/medias/standard/c/h/christelleferron/artwork/10638688_le-monde-en-couleur-50x50.jpg"}
-          fotoPost={"https://www.fas.org.sg/wp-content/uploads/2018/05/200x150.png"}
-        />
+      <MainContainer>       
+        {this.state.post.map(({nome, fotoUsuario, fotoPost}) => {
+          return (<Post 
+            nome= {nome} 
+            fotoUsuario= {fotoUsuario}
+            fotoPost= {fotoPost}
+            />)
+        })} 
       </MainContainer>
     );
   }
