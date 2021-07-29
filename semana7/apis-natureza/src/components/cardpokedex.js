38a9b8f775be7aaaf9f4a1ba2482f0a10f3366c6
MatheusOliveiraSantos <matheuso.santos@hotmail.com>
2021-07-29 14:47:25 -0300

    pokédex finalizada

---
+++ semana7/apis-natureza/src/components/cardpokedex.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components' 
+
+const CardPokedex = styled.div`
+  display: flex; 
+  justify-content: center; 
+  font-family: Arial, Helvetica, sans-serif; 
+  font-size: 25px; 
+  background-color: #ff1f1f; 
+  height: 80vh; 
+`  
+export default CardPokedex
