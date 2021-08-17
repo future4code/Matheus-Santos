9d95bb947721cb26524e9be30cb9e008b030900a
MatheusOliveiraSantos <matheuso.santos@hotmail.com>
2021-08-17 17:31:37 -0300

    faltando implementar a lógica

--- semana10-projeto-labeX/semana10-projeto-labex/src/App.js
+++ semana10-projeto-labeX/semana10-projeto-labex/src/App.js
@@ -1,10 +1,41 @@
-
+import React from 'react' 
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <> 
-    
-    </>
+    <BrowserRouter> 
+      <Switch> 
+        
+        <Router path={"/adminhome"}>
+        <AdminHomePage />
+        </Router>
+
+        <Router path={"/applicationform"}>
+        <ApplicationFormPage />
+        </Router>
+
+        <Router path={"/createtrip"}>
+        <CreateTripPage />
+        </Router>
+
+        <Router path={"/home"}>
+        <HomePage /> 
+        </Router>
+
+        <Router path={"/listtrips"}>
+        <ListTripsPage />
+        </Router>
+
+        <Router path={"/login"}>
+        <LoginPage />
+        </Router>
+
+        <Router path={"/tripdetails"}>
+        <TripDetailsPage />
+        </Router>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
